76763234bf2ee803c73633103273c7d39950175e
RockingCrimsonCat <130107956+RockingCrimsonCat@users.noreply.github.com>
2023-06-18 22:26:08 +0300

    18.06

---
+++ WeatherAPI/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace KarinaAPI
+{
+    public class Constants
+    {
+        public static string adress = "https://date.nager.at";
+        public static string Connect = "Host=localhost;Username=postgres;Password=strongpass;Database=postgres";
+    }
+}
---
+++ WeatherAPI/Data/UserEventDB.cs
@@ -0,0 +1,9 @@
+﻿namespace KarinaAPI.Data
+{
+    public class UserEventDB
+    {
+        public string Date { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+    }
+}
